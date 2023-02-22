d9138e6515919691306c1824ed03bac08bd06952
Robin Stenlund <robin.stenlund@gmail.com>
2023-02-22 10:33:38 +0100

    Kopplingstabell CarUser

---
+++ Biluthyrning/Models/CarUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Biluthyrning.Models
+{
+	public class CarUser
+	{
+		public int Id { get; set; }
+		public int CarId { get; set; }
+		public int UserId { get; set; }
+		public DateTime Booked { get; set; }
+	}
+}
