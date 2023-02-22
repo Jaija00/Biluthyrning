6938eb29d9b64cf4f09583c6918e6092920d1013
Robin Stenlund <robin.stenlund@gmail.com>
2023-02-22 11:05:29 +0100

    Added empty props

--- Biluthyrning/Models/User.cs
+++ Biluthyrning/Models/User.cs
@@ -5,8 +5,9 @@
 		public int Id { get; set; }
 		public bool Blacklist { get; set; }
 		public bool IsAdmin { get; set; }
-		public string Name { get; set; }
-		public string Email { get; set; }
-		public string PhoneNumber { get; set; }
+		public string FirstName { get; set; } = "";
+		public string LastName { get; set; } = "";
+		public string Email { get; set; } = "";
+		public string PhoneNumber { get; set; } = "";
 	}
 }
