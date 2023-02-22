5047da9c0955ce7bf53c68b81d444e2b5d130267
Robin Stenlund <robin.stenlund@gmail.com>
2023-02-22 10:33:30 +0100

    User Model

---
+++ Biluthyrning/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Biluthyrning.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public bool Blacklist { get; set; }
+		public bool IsAdmin { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+	}
+}
