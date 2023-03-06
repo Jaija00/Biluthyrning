6ae049ef842777b879ded3137e15fe6f4cfde75a
Nima <nimamola@gmail.com>
2023-03-06 14:37:39 +0100

    Added Migration

---
+++ Biluthyrning/Migrations/20230306123403_Changed from bool to string Gear.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biluthyrning.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedfrombooltostringGear : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Manual",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gear",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gear",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Manual",
+                table: "Cars",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
