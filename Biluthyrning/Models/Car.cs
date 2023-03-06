46739794803d60c9c25cb65f543b72dcd4f273d0
Nima <nimamola@gmail.com>
2023-03-06 13:22:49 +0100

    Lagt till Displayname

--- Biluthyrning/Models/Car.cs
+++ Biluthyrning/Models/Car.cs
@@ -1,14 +1,23 @@
-﻿namespace Biluthyrning.Models
+﻿using System.ComponentModel;
+
+namespace Biluthyrning.Models
 {
 	public class Car
 	{
 		public int CarId { get; set; }
+		[DisplayName ("Modell")]
 		public string Name { get; set; } = "";
-		public string Brand { get; set; } = "";
-		public string Color { get; set; } = "";
-		public bool Manual { get; set; }
-		public string FuelType { get; set; } = "";
-		public string Size { get; set; } = "";
-		public int Price { get; set; }
+        [DisplayName("Märke")]
+        public string Brand { get; set; } = "";
+        [DisplayName("Färg")]
+        public string Color { get; set; } = "";
+        [DisplayName("Växeltyp")]
+        public bool Manual { get; set; }
+        [DisplayName("Drivmedel")]
+        public string FuelType { get; set; } = "";
+        [DisplayName("Storlek")]
+        public string Size { get; set; } = "";
+        [DisplayName("Pris")]
+        public int Price { get; set; }
 	}
 }
