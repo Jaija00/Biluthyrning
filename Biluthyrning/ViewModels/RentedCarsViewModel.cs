282ed9fe65596f82583de95cf3c5820b624fbe1f
mahsakooshesh <mahsa.kooshesh@studerande.plushogskolan.se>
2023-03-06 21:13:57 +0100

    Given ID:s to Models

--- Biluthyrning/ViewModels/RentedCarsViewModel.cs
+++ Biluthyrning/ViewModels/RentedCarsViewModel.cs
@@ -4,6 +4,7 @@ namespace Biluthyrning.ViewModels
 {
     public class RentedCarsViewModel
     {
+        public int Id { get; set; }
         public int CarId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
