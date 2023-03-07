fc77a95e209d1162299e2418eef6e082386f4224
Emeli <emeli.karlstrom@gmail.com>
2023-03-07 13:59:47 +0100

    Fixat med design för användare och bilar

--- Biluthyrning/Models/User.cs
+++ Biluthyrning/Models/User.cs
@@ -1,13 +1,19 @@
-﻿namespace Biluthyrning.Models
+﻿using System.ComponentModel;
+
+namespace Biluthyrning.Models
 {
 	public class User
 	{
 		public int UserId { get; set; }
 		public bool Blacklist { get; set; }
 		public bool IsAdmin { get; set; }
-		public string FirstName { get; set; } = "";
-		public string LastName { get; set; } = "";
-		public string Email { get; set; } = "";
-		public string PhoneNumber { get; set; } = "";
+        [DisplayName("Förnamn")]
+        public string FirstName { get; set; } = "";
+        [DisplayName("Efternamn")]
+        public string LastName { get; set; } = "";
+        [DisplayName("Mailadress")]
+        public string Email { get; set; } = "";
+        [DisplayName("Telefonnummer")]
+        public string PhoneNumber { get; set; } = "";
 	}
 }
