11fb3108b738260b53746da282d45972aaa61e8a
RStenlund <robin.stenlund@gmail.com>
2023-03-13 08:56:48 +0100

    Fix

--- Biluthyrning/Controllers/BookingsController.cs
+++ Biluthyrning/Controllers/BookingsController.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Biluthyrning.Data;
 using Biluthyrning.Models;
 using Biluthyrning.ViewModels;
+using System.Drawing;
+using System.Runtime.ConstrainedExecution;
 
 namespace Biluthyrning.Controllers
 {
@@ -37,13 +39,42 @@ namespace Biluthyrning.Controllers
             return View();
         }
 
+        // GET: Cars/BookingCarFirstView
+        public async Task<IActionResult> BookingCarFirstView()
+        {
+            ViewBag.Brand = new SelectList(await carRepository.GetAllAsync(), "CarId", "Brand");
+            ViewBag.Color = new SelectList(await carRepository.GetAllAsync(), "CarId", "Color");
+            ViewBag.CarGear = new SelectList(await carRepository.GetAllAsync(), "CarId", "Gear");
+            ViewBag.Fuel = new SelectList(await carRepository.GetAllAsync(), "CarId", "FuelType");
+            ViewBag.CarSize = new SelectList(await carRepository.GetAllAsync(), "CarId", "Size");
+
+            return View();
+        }
 
         public async Task<IActionResult> FilterList()
         {
             return View();
         }
 
+        //public async Task<IActionResult> SearchedCarToBook(string name, string brand, string color, string gear, string fuel, string size)
+        //{
+        //    foreach (var car in await carRepository.GetAllAsync())
+        //    {
+        //        if (car.Gear == gear && car.FuelType == fuel && car.Size == size)
+        //        {
+        //            return View(car);
+        //        }
+        //        else
+        //        {
+        //            return NotFound();
+        //        }
+        //    }
+
+        //    return View();
+        //}
+
         // GET: Bookings/AvailableCars
+
         public async Task<IActionResult> AvailableCars(DateTime startDate, DateTime endDate)
         {
             var availableCarsVM = new List<AvailableCarsViewModel>();
@@ -54,10 +85,13 @@ namespace Biluthyrning.Controllers
                 post.Booking = await bookingRepository.GetByIdAsync(car.CarId);
                 if (post.Booking != null)
                 {
-                    if ((startDate >= post.Booking.End && endDate >= post.Booking.End) || (endDate <= post.Booking.Start && startDate <= post.Booking.Start))
-                    {
-                        availableCarsVM.Add(post);
-                    }
+                    //if ((startDate >= post.Booking.End && endDate >= post.Booking.End) || (endDate <= post.Booking.Start && startDate <= post.Booking.Start))
+                    //    if ((searchCarViewModel.DatePicker.StartDate >= post.Booking.End && searchCarViewModel.DatePicker.EndDate >= post.Booking.End) ||
+                    //        (searchCarViewModel.DatePicker.EndDate <= post.Booking.Start && searchCarViewModel.DatePicker.StartDate <= post.Booking.Start
+                    //        && post.Car.Gear == c.Gear && post.Car.FuelType == c.FuelType && post.Car.Size == c.Size))
+                        {
+                            availableCarsVM.Add(post);
+                        }
                 }
                 else
                 {
@@ -152,21 +186,21 @@ namespace Biluthyrning.Controllers
             }
             return View(booking);
         }
-       
+
         // GET: Bookings/Delete/5
         public async Task<IActionResult> Delete(int id)
         {
-         
-            
-             var d = new DetailsUserViewModel();
-             d.Booking =await bookingRepository.GetByIdAsync(id);
-             d.Car=await carRepository.GetByIdAsync(d.Booking.CarId);
+
+
+            var d = new DetailsUserViewModel();
+            d.Booking = await bookingRepository.GetByIdAsync(id);
+            d.Car = await carRepository.GetByIdAsync(d.Booking.CarId);
             d.User = await userRepository.GetByIdAsync(d.Booking.UserId);
 
-           
-            
+
+
             return View(d);
-  
+
         }
 
         // POST: Bookings/Delete/5
@@ -185,7 +219,7 @@ namespace Biluthyrning.Controllers
                 {
                     return View();
                 }
-                return RedirectToAction("Details","Users",new {id=booking.UserId});
+                return RedirectToAction("Details", "Users", new { id = booking.UserId });
             }
             return View(booking);
 
